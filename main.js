56585b0b0b6223a588afb29842c1c196b97f271c
Tomas_Gualtieri <tomi.meridiano@gmail.com>
2023-11-19 22:20:35 -0300

    Segunda entrega de js

--- main.js
+++ main.js
@@ -1,45 +1,32 @@
-//Estacion de servicio.
-function saludar() {
-    let nombre = prompt("¿hola como es tu nombre?")
-    alert("Bienvenido, " + nombre)
-}
-saludar()
+//Control de llegada para trabajadores de una peluqueria.
 
-const tipoCombustible = prompt("¿Qué combustible vas cargar nafta o gasoil?")
-let litros = parseFloat(prompt("¿Cuántos litros vas cargar?"))
-
-const precioNafta = 257.13
-const precioGasoil = 491.27
-
-let costoCombustible
-
-if (tipoCombustible.toLowerCase() === "nafta") {
-    costoCombustible = precioNafta * litros
-} else if (tipoCombustible.toLowerCase() === "gasoil") {
-    costoCombustible = precioGasoil * litros
-} else {
-    alert("No tenemos ese combustible, solo tenemos nafta o gasoil.")
+const Trabajador = function(nombre,entrada,puesto,){
+    this.nombre = nombre;
+    this.entrada = entrada;
+    this.puesto = puesto;
 }
 
-alert("El monto final a pagar es de $" + costoCombustible.toFixed(2))
+const hoy = new Date()
+let llegada = hoy.toTimeString() 
 
-const metodoPago = prompt("¿Cómo vas a pagar en efectivo o con tarjeta?")
+let trabajador1 = new Trabajador("Carlos",llegada, "limpieza")
+let trabajador2 = new Trabajador("Maria", llegada, "atencion al publico")
+let trabajador3 = new Trabajador("Jose", llegada, "peluquero")
+let trabajador4 = new Trabajador("Juan", llegada, "peluquero")
+let trabajador5 = new Trabajador("Zoe", llegada, "encargada")
 
-let totalAPagar
+let lista = [trabajador1,trabajador2,trabajador3,trabajador4,trabajador5]
 
-if (metodoPago.toLowerCase() === "efectivo") {
-    const dineroRecibido = parseFloat(prompt("Igrese el dinero"))
-    totalAPagar = costoCombustible
+function listaDeTrabajadores(){
+    let persona = prompt("Nombre del trabajador...").toUpperCase().trim()
+    let resultado = lista.filter((x)=>x.nombre.toUpperCase().includes(persona))
 
-    if (dineroRecibido < totalAPagar) {
-        alert("Dinero insuficiente.")
-    } else {
-        const vuelto = dineroRecibido - totalAPagar
-        alert("Gracias,  Tu cambio es de $" + vuelto.toFixed(2) + ".")
+    if(resultado.length > 0){
+        console.table(resultado)
+    }else{
+        alert("No figura en el sistema")
     }
-} else if (metodoPago.toLowerCase() === "tarjeta") {
-    totalAPagar = costoCombustible * 1.1
-    alert("Gracias, El total a pagar con tarjeta es de $" + totalAPagar.toFixed(2) + ".")
-} else {
-    alert("Método de pago no válido.")
+    
 }
+
+listaDeTrabajadores()
